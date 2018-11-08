81d21911264d2f0ddc82f02c046d539e0923e5a3
“Ben <ben.fondon@gmail.com>
2018-11-08 16:57:59 -0600

    Fixed JS so that initial buttons show and pull gifs when clicked. Also, new buttons can be added and they pull gifs as well.

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var animals=["dog", "cat", "hedgehog", "racoon", "turtle"];
 
         function addButtons(){
-            $(".gif-btn").empty();
+            $("#buttons-view").empty();
             for(var i=0; i<animals.length; i++){
                 var button = $("<button>");
                 button.addClass("gif-btn");
@@ -12,9 +12,8 @@ $(document).ready(function(){
                 button.text(animals[i]);
                 $("#buttons-view").append(button);
             }
-        }
-    addButtons();
-
+        };
+        
     function showGifs(){
 
     var gif = $(this).attr("data-name");
@@ -27,32 +26,21 @@ $(document).ready(function(){
         var result = response.data;
         for(i=0; i<result.length; i++){
         var gifImg = $("<img>");
-        gifImg.attr("src", result[i].images.fixed_width);
-        gifImg.attr("id",);
+        gifImg.attr("class", "thing");
+        gifImg.attr("src", result[i].images.fixed_width.url);
         $("#gif-container").prepend(gifImg);
         console.log(result);
-            };
-        })    
-    })    
-    showGifs(); 
-    console.log('E is the best and the patriots are dynasty!')
-}); 
+        };
+    });
+    };
+    addButtons();
 
+    $("#add-gif").on("click", function(event){
+        event.preventDefault();
+        var newGif= $("#animal-input").val();
+        animals.push(newGif);
+        addButtons();
+    })
 
-//1. When the page loads, populate pre-existing buttons.
-        //A. Need an onload function.
-        //B. Need a variable that stores the pre-existing buttons.
-        //C. Every time the page loads, empty all existing buttons and reload the new page. This prevents the same buttons being re-loaded over and over.
-        //D. Create a for loop that goes through the array and creates buttons for each. 
-            // d. Need a variable to store the button tag. 
-            // e. Add class using .addClass.
-            // f. Add attributes .attr - assigns a specific identifier to each button.
-            // g. Add text to each button - what is displayed - this is equal to animals[i].
-            // h. Go into html, search for a button id, and append/shove button to the end of the array.
-            // i. Call the function.
-//2. When a button is clicked, put gifs in a container.
-    //A. Need an event listener (doc.onclick) for any button clicked.
-    //B. When a button is clicked, run a function.
-    //C. 
-
-//3. When you submit an input, a new button is created that behaves like the original buttons.
+    $(document).on("click",".gif-btn",showGifs);
+})
